26b4957b9863c4612880b3c56b2f058d7634a700
Aodhagan Collins <aodhaganc@gmail.com>
2017-02-19 23:23:26 +0000

    Code: StoryStorage initial commit

---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss']
+})
+
+export class LandingComponent {
+  signUp(event: MouseEvent) {
+    alert('Still working on this sorry - come back soon!');
+  }
+
+  signIn(event: MouseEvent) {
+    alert('Still working on this sorry - come back soon!');
+  }
+};
